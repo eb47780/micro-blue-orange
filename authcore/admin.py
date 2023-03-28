1e0f943a86eb401d83136fd2fdab7a6768ce6b4b
endritber <endritpb@gmail.com>
2023-03-28 16:41:18 +0200

    add user and user client models

--- authcore/admin.py
+++ authcore/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from administration.admin import admin_site
+from authcore.models import User
 
 # Register your models here.
+class UserAdmin(DjangoUserAdmin):
+    pass
+
+admin_site.register(User, UserAdmin)
