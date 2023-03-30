9230fb8af40d17ca25efa32cf62ac68f990569ae
endritber <endritpb@gmail.com>
2023-03-30 14:52:25 +0200

    Added address creation, update and delete for current user

--- core/permissions.py
+++ core/permissions.py
@@ -4,3 +4,6 @@ class IsClientOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.user_id == request.user.id
   
+class IsAddressOwnerDetail(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.customer_id == request.user.id
