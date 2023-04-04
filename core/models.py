1e2e56c1c4ad9d2fcd3e40cf456517d13acb94c1
endritber <endritpb@gmail.com>
2023-04-04 10:10:14 +0200

    Added Checkout Items

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from authcore.models import UserClient
 from common.models import BaseCustomer, AutoCreateUpdateMixin
+from payment.models import PaymentMethod
 from autoslug import AutoSlugField
 import uuid
 import os
@@ -66,3 +67,55 @@ class Product(AutoCreateUpdateMixin):
 
     def __str__(self) -> str:
         return self.title
+
+
+class Status(AutoCreateUpdateMixin):
+    STATUS = (
+        ('Processing Purchase', 'Processing Purchase'),
+        ('Approved Purchase', 'Approved Purchase'),
+        ('Purchase Denied', 'Purchase Denied'),
+        ('Purchase Sent', 'Purchase Sent')
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    message = models.CharField(max_length=30, choices=STATUS)
+
+    class Meta:
+        verbose_name = 'status'
+        verbose_name_plural = 'status'
+
+    def __str__(self):
+        return self.message
+
+
+class Checkout(AutoCreateUpdateMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='user_client_checkout')
+    address = models.ForeignKey(Address, on_delete=models.PROTECT)
+    payment_method = models.ForeignKey(PaymentMethod, on_delete=models.PROTECT, verbose_name='payment_method')
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, null=True, related_name='status')
+    bank_slip_url = models.URLField(blank=True, null=True, verbose_name='billet_url')
+    remote_id = models.CharField(max_length=255, blank=True, null=True, default=None, verbose_name='remote_invoice_id', help_text='remote invoice id at the payment gateway')
+
+    class Meta:
+        verbose_name = 'checkout'
+
+    @property
+    def total(self):
+        sum = 0
+        for item in self.checkout_items.all():
+            sum += item.price * item.quantity
+        return sum
+
+
+class CheckoutItem(AutoCreateUpdateMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    checkout = models.ForeignKey(Checkout, on_delete=models.CASCADE, related_name='checkout_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='checkout_item_product')
+    quantity = models.PositiveSmallIntegerField(verbose_name='quantity')
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='price')
+
+    class Meta:
+        verbose_name = 'checkout item'
+
+    def __str__(self):
+        return f'Email {self.checkout.customer.email} Date {self.created_at}'
