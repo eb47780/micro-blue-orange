b0a8f66c6a6351e320110d1e85c3a069606d3c32
endritber <endritpb@gmail.com>
2023-04-04 11:17:09 +0200

    Added Checkout and Payment Serializers

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,11 @@
 import core.models as models
 from authcore.models import UserClient
+from payment.serializers import PaymentMethodSerializer
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.db import transaction, IntegrityError
+from django.forms.models import model_to_dict
+from config.celery import _publish
 
 
 class ClientSerializer(serializers.ModelSerializer):
@@ -75,3 +79,91 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def get_image_url(self, obj):
         return obj.image.url
+
+
+class StatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Status
+        fields = '__all__'
+
+
+class StatusDetailSerializer(serializers.ModelSerializer):
+    message = serializers.CharField(style={'input_type': 'charfield'})
+
+    class Meta:
+        model = models.Status
+        fields = ['url', 'message']
+
+
+class CheckoutItemSerializer(serializers.ModelSerializer):
+    title = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = models.CheckoutItem
+        fields = '__all__'
+        read_only_fields = ['checkout']
+
+    def get_title(self, obj):
+        return obj.product.title
+
+
+class CheckoutSerializer(serializers.ModelSerializer):
+    items = CheckoutItemSerializer(many=True)
+    total = serializers.SerializerMethodField(read_only=True)
+    card_hash = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = models.Checkout
+        fields = '__all__'
+
+    def get_total(self, obj):
+        return obj.total
+
+    def create(self, validated_data):
+        payment_method = validated_data['payment_method']
+        card_hash = validated_data.pop('card_hash')
+
+        try:
+            with transaction.atomic():
+                items = list(validated_data.pop('items'))
+                checkout = models.Checkout.objects.create(**validated_data)
+                checkout_items = []
+                for item in items:
+                    item['checkout'] = checkout
+                    checkout_items.append(models.CheckoutItem(**item))
+                checkout.items = checkout.checkout_item.bulk_create(checkout_items)
+
+                payload = {
+                    'customer': ClientSerializer(validated_data['customer']).data['id'],
+                    'payment_method': PaymentMethodSerializer(payment_method).data['name'],
+                    'checkout_id': CheckoutSerializer(checkout).data['id'],
+                    'card_hash': card_hash
+                }
+
+                _publish(message=payload, routing_key='payment')
+                return checkout
+        except IntegrityError as e:
+            raise serializers.ValidationError({"detail": e})
+
+
+class CheckoutDetailSerializer(serializers.ModelSerializer):
+    items = serializers.SerializerMethodField(read_only=True)
+    status = serializers.SerializerMethodField(read_only=True)
+    total = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = models.Checkout
+        fields = '__all__'
+
+    def get_total(self, obj):
+        return obj.total
+
+    def get_items(self, obj):
+        return [{
+            'title': check_item.product.title,
+            'quantity': check_item.quantity,
+            'price': check_item.price
+        } for check_item in obj.checkout_items.all()]
+
+    def status(self, obj):
+        return model_to_dict(models.Status.objects.get(id=obj.status.id))
