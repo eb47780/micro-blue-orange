e190fb656362c418f7f2e741a9882845a97c32a2
endritber <endritpb@gmail.com>
2023-04-04 11:18:43 +0200

    Added Receivers for Checkout

---
+++ core/receivers.py
@@ -0,0 +1,7 @@
+from common.ModelObserver import ModelObserver
+from core.serializers import CheckoutSerializer
+from core.models import Checkout
+
+
+checkout_observer = ModelObserver(sender=Checkout, serializer=CheckoutSerializer)
+checkout_observer.register()
