081bee67b51c6a7653f650a5099a2b9492dad790
endritber <endritpb@gmail.com>
2023-04-04 21:55:44 +0200

    added simulation

--- payment/process_payment.py
+++ payment/process_payment.py
@@ -1,8 +1,15 @@
-from hashids import Hashids
-import time
+import stripe
+from payment.models import StripeGateway
+from core.models import Customer, Checkout
+from config import settings
 
 
-def proccess_payment_simulation(payment_method, card_hash):
-    time.sleep(60)
-    hashids = Hashids(alphabet='abcdefghijklmnopqrstuvwxyz1234567890', min_length=22)
-    return hashids.encode(1)
+def process_payment(validated_data):
+    obj = StripeGateway.objects.filter().first()
+    secret_key = getattr(obj, 'secret_key')
+    stripe.api_key = secret_key.strip()
+
+    customer = Customer.objects.filter(id=validated_data['customer']).first()
+    checkout = Checkout.objects.filter(id=validated_data['checkout_id']).first()
+
+    return None
