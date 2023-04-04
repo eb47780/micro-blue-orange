e84db8ed1f5fc0a145d398efd1e24c57fca20252
endritber <endritpb@gmail.com>
2023-04-04 09:40:56 +0200

    added entities to administrator service

--- administration/admin.py
+++ administration/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.forms import AuthenticationForm
+from core import models
 
 
 class AdminSite(admin.AdminSite):
@@ -9,3 +10,22 @@ class AdminSite(admin.AdminSite):
     login_form = AuthenticationForm
 
 admin_site = AdminSite(name='admin_site')
+
+
+@admin.register(models.Category, site=admin_site)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Product, site=admin_site)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'stock')
+    search_fields = ('title', )
+
+@admin.register(models.Customer, site=admin_site)
+class CustomerAdmin(admin.ModelAdmin):
+    exclude = ['user']
+    list_display = ('name', 'email')
+
+@admin.register(models.Address, site=admin_site)
+class AddressAdmin(admin.ModelAdmin):
+    pass
