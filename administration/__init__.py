00494b9c48bc681a9bf97f345216fa5cd3c09efc
endritber <endritpb@gmail.com>
2023-04-05 15:51:51 +0200

    deleted old files

--- administration/__init__.py
+++
@@ -1,7 +0,0 @@
-from django.utils.module_loading import autodiscover_modules
-
-
-def autodiscover():
-   autodiscover_modules('admin')
-
-default_app_config = 'administration.apps.AdminConfig'
--- administration/migrations/__init__.py
+++
@@ -1,6 +0,0 @@
-from django.utils import module_loading
-
-def autodiscover():
-   module_loading.autodiscover_modules('admininstration')
-
-default_app_config = 'administration.apps.AdminConfig'
