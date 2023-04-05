486e27c0d0c2ff09d0af2f67a89da88cd0a97c64
endritber <endritpb@gmail.com>
2023-04-05 15:50:53 +0200

    remove authcore

--- authcore/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
-from administration.admin import admin_site
-from authcore.models import User
-
-
-class UserAdmin(DjangoUserAdmin):
-    pass
-
-admin_site.register(User, UserAdmin)
