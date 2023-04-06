14ec8e0ee6d5a7a4f97adf0dbf55a7a61c926ec0
endritber <endritpb@gmail.com>
2023-04-06 08:57:13 +0200

    Added 2 blank lines

--- product_service/fixtures/management/commands/reset_db.py
+++ product_service/fixtures/management/commands/reset_db.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from django.db import connection
 from django.conf import settings
 
+
 class Command(BaseCommand):
 
     def handle(self, **options):
