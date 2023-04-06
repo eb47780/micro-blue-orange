5903b41d16388e00770890258399b484f7403c01
endritber <endritpb@gmail.com>
2023-04-06 12:13:08 +0200

    Change Checkout name

--- checkout_service/checkout/urls.py
+++ checkout_service/checkout/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('api/status/<str:pk>', StatusDetail.as_view(), name=StatusDetail.name),
 
     # api payment
-    path('api/checkouts/v1', CheckoutListCreateView.as_view(), name=CheckoutListCreateView.name),
+    path('api/checkouts/v1', CheckoutCreateView.as_view(), name=CheckoutCreateView.name),
     path('api/checkout/<str:pk>', CheckoutDetail.as_view(), name=CheckoutDetail.name),
     path('api/checkoutitems/v1', CheckoutItemCreateView.as_view(), name=CheckoutItemCreateView.name),
     path('api/checkoutitem/<str:pk>', CheckoutItemDetail.as_view(), name=CheckoutItemDetail.name),
