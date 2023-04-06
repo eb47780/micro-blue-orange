6bb86e137dcfc9b6fd96ef2571b01fae8395337f
endritber <endritpb@gmail.com>
2023-04-06 08:57:46 +0200

    Added 2 blank lines

--- product_service/product/admin.py
+++ product_service/product/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from product.models import Product, Category
 
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'price', 'stock')
     search_fields = ('title', )
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
