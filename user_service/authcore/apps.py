df5ef1a7b8d001943c101dffe2fcaede5da5d632
endritber <endritpb@gmail.com>
2023-04-06 08:51:46 +0200

    Changed verbose_name

--- user_service/authcore/apps.py
+++ user_service/authcore/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AuthcoreConfig(AppConfig):
     name = 'authcore'
-    verbose_name = 'Authentication / Authorization Service'
+    verbose_name = 'User Service'
