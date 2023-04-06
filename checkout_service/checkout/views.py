74a002e26c9f5cdf83f6fc4d954037825666fd10
endritber <endritpb@gmail.com>
2023-04-06 12:26:14 +0200

    Remove unused imports

--- checkout_service/checkout/views.py
+++ checkout_service/checkout/views.py
@@ -3,7 +3,6 @@ from rest_framework.reverse import reverse
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
-from rest_framework import permissions
 from checkout import models, serializers
 import checkout.permissions as custom_permissions
 
