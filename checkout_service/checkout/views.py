f6ecb570f3b2205bb9be6618c3ea9a914e53f82e
endritber <endritpb@gmail.com>
2023-04-06 12:13:41 +0200

    Provide custom permission classes

--- checkout_service/checkout/views.py
+++ checkout_service/checkout/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.reverse import reverse
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.generics import ListAPIView, RetrieveAPIView, ListCreateAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
 from rest_framework import permissions
 from checkout import models, serializers
 import checkout.permissions as custom_permissions
@@ -15,7 +15,7 @@ class ApiRoot(APIView):
     def get(self, request, *args, **kwargs):
         data = {
             "status": reverse(ApiRoot.BASE_REVERSE+StatusListView.name, request=request),
-            "checkouts": reverse(ApiRoot.BASE_REVERSE+CheckoutListCreateView.name, request=request),
+            "checkouts": reverse(ApiRoot.BASE_REVERSE+CheckoutCreateView.name, request=request),
             "checkoutitems": reverse(ApiRoot.BASE_REVERSE+CheckoutItemCreateView.name, request=request),
         }
         return Response(data, status=status.HTTP_200_OK)
@@ -25,34 +25,28 @@ class StatusListView(ListAPIView):
     name = 'status-list-view'
     queryset = models.Status.objects.get_queryset()
     serializer_class = serializers.StatusSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [custom_permissions.IsAuthenticated]
 
 
 class StatusDetail(RetrieveAPIView):
     name = 'status-detail'
     queryset = models.Status.objects.get_queryset()
     serializer_class = serializers.StatusDetailSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [custom_permissions.IsAuthenticated]
 
 
-class CheckoutListCreateView(ListCreateAPIView):
-    name = 'checkout-list-create-view'
+class CheckoutCreateView(CreateAPIView):
+    name = 'checkout-create-view'
     queryset = models.Checkout.objects.get_queryset()
     serializer_class = serializers.CheckoutSerializer
-    permission_classes = [custom_permissions.IsAuthenticated, custom_permissions.IsCheckoutOwner]
-
-    def list(self, request, *args, **kwargs):
-        current_user = request.user
-        checkouts = models.Checkout.objects.filter(customer=current_user.id)
-        serializer_checkout = serializers.CheckoutDetailSerializer(checkouts, many=True)
-        return Response(serializer_checkout.data, status=status.HTTP_200_OK)
+    permission_classes = [custom_permissions.IsAuthenticated]
 
 
 class CheckoutDetail(RetrieveAPIView):
     name = 'checkout-detail'
     queryset = models.Checkout.objects.get_queryset()
     serializer_class = serializers.CheckoutDetailSerializer
-    permission_classes = []
+    permission_classes = [custom_permissions.IsAuthenticated]
 
 
 class CheckoutItemCreateView(CreateAPIView):
@@ -66,4 +60,4 @@ class CheckoutItemDetail(RetrieveAPIView):
     name = 'checkout-item-detail'
     queryset = models.CheckoutItem.objects.get_queryset()
     serializer_class = serializers.CheckoutItemSerializer
-    permission_classes = [custom_permissions.IsAuthenticated, custom_permissions.IsCheckoutItemOwner]
+    permission_classes = [custom_permissions.IsAuthenticated]
