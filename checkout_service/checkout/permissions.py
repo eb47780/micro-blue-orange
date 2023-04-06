05a219b95fb0623333999a95cd682e657f36e7ff
endritber <endritpb@gmail.com>
2023-04-06 12:12:32 +0200

    Added token verification permission

--- checkout_service/checkout/permissions.py
+++ checkout_service/checkout/permissions.py
@@ -1,21 +1,20 @@
 from rest_framework import permissions
+import requests
 
 
 class IsAuthenticated(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return request.user.is_authenticated
+    def has_permission(self, request, view):
+        token = str(request.META['HTTP_AUTHORIZATION'].split()[1])
+        response = requests.post('http://127.0.0.1:8001/api/token/verify/', data={"token": token})
+        if response.status_code == 200:
+            return True
+        elif response.status_code == 401:
+            return False
 
 
 class IsCheckoutOwnerDetail(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.customer == request.user.id
-
-
-class IsCheckoutOwner(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        print(obj)
-        return obj.customer == request.user.id
-
 
 class IsCheckoutItemOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
