39a3970b3d2aae0bdf54411da76f07c10306a380
endritber <endritpb@gmail.com>
2023-04-07 18:16:46 +0200

    Change host to container name

--- checkout_service/checkout/permissions.py
+++ checkout_service/checkout/permissions.py
@@ -5,7 +5,7 @@ import requests
 class IsAuthenticated(permissions.BasePermission):
     def has_permission(self, request, view):
         token = str(request.META['HTTP_AUTHORIZATION'].split()[1])
-        response = requests.post('http://127.0.0.1:8001/api/token/verify/', data={"token": token})
+        response = requests.post('http://user-service:8000/api/token/verify/', data={"token": token})
         if response.status_code == 200:
             return True
         elif response.status_code == 401:
