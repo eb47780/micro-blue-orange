8f29e68e8c130999a95d6d3959fbbcc3bdb59a13
endritber <endritpb@gmail.com>
2023-04-07 14:12:41 +0200

    Added Consumer

--- checkout_service/checkout_service_config/celery.py
+++ checkout_service/checkout_service_config/celery.py
@@ -1,13 +1,14 @@
 import logging
 import os
 
-from celery import Celery
-
+from celery import Celery, bootsteps
+import kombu
+from django.db import transaction
 
 logger = logging.getLogger(__name__)
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS MODULE', 'checkout_service_config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'checkout_service_config.settings')
 
 app = Celery('proj')
 
@@ -36,3 +37,43 @@ def _publish(message, routing_key, exchange):
             routing_key=routing_key,
             exchange=exchange
         )
+
+with rabbitmq_conn() as conn:
+    queue = kombu.Queue(
+        name='queue-checkout',
+        exchange='checkout',
+        routing_key='checkout_service',
+        channel=conn,
+        durable=True
+    )
+    queue.declare()
+
+
+class CheckoutConsumer(bootsteps.ConsumerStep):
+    def get_consumers(self, channel):
+        return [
+            kombu.Consumer(
+                channel,
+                queues=[queue],
+                callbacks=[self.handle_message],
+                accept=['json']
+            )
+        ]
+
+    def handle_message(self, data, message):
+        try:
+            with transaction.atomic():
+                from checkout.models import Checkout, Status
+                checkout = Checkout.objects.filter(id=data['checkout_id']).first()
+                status = Status.objects.filter(id=data['status_id']).first()
+                checkout.status = status
+                checkout.remote_id = data['remote_invoice_id']
+                checkout.save()
+                logger.info('Checkout Service Status Completed')
+        except Exception as e:
+            logger.exception(e)
+
+        message.ack()
+
+
+app.steps['consumer'].add(CheckoutConsumer)
