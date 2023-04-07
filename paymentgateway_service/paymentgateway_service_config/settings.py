f8a5383ae37880bad83dfbec75815c56f7a4c31b
endritber <endritpb@gmail.com>
2023-04-07 12:34:49 +0200

    Added BROKER URL

--- paymentgateway_service/paymentgateway_service_config/settings.py
+++ paymentgateway_service/paymentgateway_service_config/settings.py
@@ -131,6 +131,11 @@ USE_I18N = True
 USE_TZ = True
 
 
+if not LOCAL:
+    CELERY_BROKER_URL = env('RABBITMQ_URI')
+else:
+    CELERY_BROKER_URL = "amqp://admin:admin@localhost:5672"
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
