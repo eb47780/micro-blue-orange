b8cd7ad9a41f8747e72b36572de8606b29154a51
endritber <endritpb@gmail.com>
2023-04-30 19:24:40 +0200

    flake8

--- paymentgateway_service/payment/views.py
+++ paymentgateway_service/payment/views.py
@@ -11,36 +11,36 @@ stripe.api_key = settings.STRIPE_API_KEY
 
 @csrf_exempt
 def stripe_webhook(request):
-  payload = request.body
-  sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-  event = None
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
 
-  try:
-    event = stripe.Webhook.construct_event(
-      payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
-    )
-  except ValueError as e:
-    logging.exception(e)
-    return HttpResponse(status=400)
-  except stripe.error.SignatureVerificationError as e:
-    logging.exception(e)
-    return HttpResponse(status=400)
-  
-  if event['type'] == 'checkout.session.completed':
-    session = stripe.checkout.Session.retrieve(
-      event['data']['object']['id'],
-      expand=['line_items'],
-    )
-    
-    customer_email = session['metadata']['customer']
-    product = session['metadata']['product']
-    address = session['metadata']['address']
-    
-    send_mail(
-      subject=f'Purchase at Blue Orange',
-      message= f'Thank you for making a purchase at Blue Orange. Here is/are the product/s you ordered {product} at address {address}',
-      recipient_list=[customer_email],
-      from_email="endritpb@gmail.com"
-    )
+    try:
+        event = stripe.Webhook.construct_event(
+          payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError as e:
+        logging.exception(e)
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        logging.exception(e)
+        return HttpResponse(status=400)
 
-  return HttpResponse(status=200)
+    if event['type'] == 'checkout.session.completed':
+        session = stripe.checkout.Session.retrieve(
+          event['data']['object']['id'],
+          expand=['line_items'],
+        )
+
+        customer_email = session['metadata']['customer']
+        product = session['metadata']['product']
+        address = session['metadata']['address']
+
+        send_mail(
+          subject='Purchase at Blue Orange',
+          message=f'Thank you for making a purchase at Blue Orange. Here is/are the product/s you ordered {product} at address {address}',
+          recipient_list=[customer_email],
+          from_email="endritpb@gmail.com"
+        )
+
+    return HttpResponse(status=200)
