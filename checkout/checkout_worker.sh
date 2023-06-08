5d9fab16e277b1ae526b97f77008088aae611107
Endrit Berisha <120720343+endritber@users.noreply.github.com>
2023-06-08 12:23:13 +0200

    refactor main (#16)

---
+++ checkout/checkout_worker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /usr/src/checkout
+
+dockerize -wait tcp://database_schema:5432 -wait tcp://amqp:15672 -timeout 2700s -wait-retry-interval 10s 
+celery -A config.celery worker -B -l INFO -s /tmp/celerybeat-schedule -n checkout_service
